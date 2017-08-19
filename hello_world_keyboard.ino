d37c78816eef0012d37654eb536a689aca87c5cd
Bartosz Wiśniewski <poprosturonin@gmail.com>
2017-08-19 16:10:03 +0200

    Add mouse mode

--- hello_world_keyboard.ino
+++ hello_world_keyboard.ino
@@ -1,18 +1,31 @@
+#define KEY_LAYER_DOWN 50920
+#define KEY_MOUSE_MODE 50921
+#define EMPTY 50922
+
 int ROWS = 5;
 int COLUMNS = 13;
 int KEYS = 65;
 
 int matrix[] = {
   KEY_ESC, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, KEY_BACKSPACE, KEY_BACKSPACE,
-  KEY_ESC, 113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93,
+  EMPTY, 113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93,
   KEY_TAB, 97, 115, 100, 102, 103, 104, 106, 107, 108, 59, KEY_RETURN, KEY_RETURN,
-  KEY_LEFT_SHIFT, 122, 120, 99, 118, 98, 110, 109, 44, 46, 47, KEY_RIGHT_SHIFT, KEY_RIGHT_SHIFT,
-  96, 96,KEY_LEFT_ALT, KEY_LEFT_CTRL, KEY_F12, KEY_SPACE, KEY_SPACE, KEY_F12, KEY_RIGHT_ALT, KEY_LEFT_ARROW, KEY_UP_ARROW, KEY_DOWN_ARROW, KEY_RIGHT_ARROW
+  KEY_LEFT_SHIFT, 122, 120, 99, 118, 98, 110, 109, 44, 46, 47, KEY_UP_ARROW, KEY_RIGHT_SHIFT,
+  KEY_MOUSE_MODE, KEY_LEFT_GUI, KEY_LEFT_CTRL, KEY_LEFT_ALT, KEY_LAYER_DOWN, KEY_SPACE, KEY_SPACE, KEY_LEFT_SHIFT, KEY_RIGHT_ALT, EMPTY, KEY_LEFT_ARROW, KEY_DOWN_ARROW, KEY_RIGHT_ARROW
 };
 
 boolean backspace_pressed = false;
 boolean space_pressed = false;
 boolean return_pressed = false;
+
+boolean mouse_mode = false;
+unsigned long mouse_timer = 0; 
+const long mouse_interval = 10; 
+
+float move_x = 0;
+float move_y = 0;
+float sensitivity = 4;
+float wheel = 0;
 
 int old[65];
 
@@ -58,7 +71,20 @@ void scan() {
 
 void loop() {
   scan();
+
+  if (mouse_mode) {
+   mouse(); 
+  }
   delay(1);
+}
+
+void mouse () {
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - mouse_timer >= mouse_interval) {
+    mouse_timer = currentMillis;
+    Mouse.move(move_x * sensitivity, move_y * sensitivity * -1, wheel);
+  }
 }
 
 void press(int key) {
@@ -68,15 +94,33 @@ void press(int key) {
       Keyboard.press(key); 
     }
   } else if (key == KEY_SPACE) {
-    if (!space_pressed) {
-      space_pressed = true;
-      Keyboard.press(key);
-    }
   } else if (key == KEY_RETURN) {
     if (!return_pressed) {
       return_pressed = true;
       Keyboard.press(key); 
     }
+  } else if (key == KEY_MOUSE_MODE) {
+    Mouse.begin();
+    mouse_mode = true;
+    move_x = 0;
+    move_y = 0;
+    wheel = 0;
+  } else if (key == KEY_UP_ARROW && mouse_mode) {
+     move_y += 1;
+  } else if (key == KEY_DOWN_ARROW && mouse_mode) {
+     move_y -= 1;
+  } else if (key == KEY_RIGHT_ARROW && mouse_mode) {
+     move_x += 1;
+  } else if (key == KEY_LEFT_ARROW && mouse_mode) {
+     move_x -= 1;
+  } else if (key == 46 && mouse_mode) {
+     wheel += 1;
+  } else if (key == EMPTY && mouse_mode) {
+     wheel -= 1;
+  } else if (key == 47 && mouse_mode) {
+    Mouse.press(MOUSE_LEFT);
+  } else if (key == KEY_RIGHT_SHIFT && mouse_mode) {
+    Mouse.press(MOUSE_RIGHT);
   } else {
     Keyboard.press(key);
   }
@@ -98,6 +142,25 @@ void release(int key) {
       return_pressed = false;
       Keyboard.release(key); 
     }
+  } else if (key == KEY_MOUSE_MODE) {
+    Mouse.end();
+    mouse_mode = false;
+  } else if (key == KEY_UP_ARROW && mouse_mode) {
+     move_y -= 1;
+  } else if (key == KEY_DOWN_ARROW && mouse_mode) {
+     move_y += 1;
+  } else if (key == KEY_RIGHT_ARROW && mouse_mode) {
+     move_x -= 1;
+  } else if (key == KEY_LEFT_ARROW && mouse_mode) {
+     move_x += 1;
+  } else if (key == 46 && mouse_mode) {
+     wheel -= 1;
+  } else if (key == EMPTY && mouse_mode) {
+     wheel += 1;
+  } else if (key == 47 && mouse_mode) {
+    Mouse.release(MOUSE_LEFT);
+  } else if (key == KEY_RIGHT_SHIFT && mouse_mode) {
+    Mouse.release(MOUSE_RIGHT);
   } else {
     Keyboard.release(key);
   }
