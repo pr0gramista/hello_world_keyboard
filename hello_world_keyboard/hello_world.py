5351d63fe18ec8cc6b58f60848d6679a1559b5e6
Bartosz Wiśniewski <poprosturonin@gmail.com>
2017-12-03 21:29:43 +0100

    Add new macros

--- hello_world_keyboard/hello_world.py
+++ hello_world_keyboard/hello_world.py
@@ -8,9 +8,9 @@ check_regex = re.compile('(press|release),([0-9]*)')
 port = 'COM3'
 rate = 9600
 
+
 def press(code):
     print('press {}'.format(code))
-
     if code == 0:
         keyboard.press_and_release("alt+f4")
     elif code == 1:
@@ -19,6 +19,20 @@ def press(code):
         os.system('"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"')
     elif code == 3:
         keyboard.write('poprosturonin@gmail.com')
+    elif code == 15:  # Q - reformat code
+        keyboard.press_and_release("ctrl+alt+l")
+    elif code == 16:  # W - move line up
+        # keyboard.press_and_release("ctrl+shift+up") need hardware support
+        pass
+    elif code == 31:  # S - move line down
+        # keyboard.press_and_release("ctrl+shift+down")
+        pass
+    elif code == 18:  # R - rename
+        keyboard.press_and_release("shift+f6")
+    elif code == 32:  # D
+        keyboard.press_and_release("ctrl+num plus")
+    elif code == 30:  # A
+        keyboard.press_and_release("ctrl+num minus")
 
 
 def release(code):
@@ -42,6 +56,7 @@ def process(port, rate):
                         press(code)
                     elif action == 'release':
                         release(code)
+
 
 while True:
     try:
